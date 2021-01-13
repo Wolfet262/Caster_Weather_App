a1eab5fb36902a4b82b8cf7bc83068bd740ae035
wolfet262 <tyler.wolfe262@outlook.com>
2021-01-13 08:28:56 -0800

    Router added and components imported

---
+++ caster/src/Components/Card.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Card(props) {
+    return (
+        <div>
+            <h1>Card Working</h1>
+        </div>
+    );
+}
+
+export default Card;
---
+++ caster/src/Components/Cards.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Card from './Card'
+
+function Cards(props) {
+    return (
+        <div>
+            <h1>Cards Working</h1>
+        </div>
+    );
+}
+
+export default Cards;
